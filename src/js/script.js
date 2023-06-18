f6fad2c5538645c869e9d6b23e47959eda30343d
Sergey Stepanenko <71725778+sstepanenkoff@users.noreply.github.com>
2023-06-18 18:34:21 +0200

    add slide block

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-      setting-name: setting-value
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        speed: 1500,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    autoplay: false,
+                    speed: 1000
+                }
+            }
+        ]
     });
-  });
+});
