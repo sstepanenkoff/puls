5bcd599da83e68398c1240908e174892e72dd752
Sergey Stepanenko <71725778+sstepanenkoff@users.noreply.github.com>
2023-06-25 15:49:08 +0200

    add catalog section

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        // autoplay: true,
-        // autoplaySpeed: 2000,
-        speed: 1500,
+        speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
@@ -13,9 +11,32 @@ $(document).ready(function(){
                     dots: true,
                     arrows: false,
                     autoplay: false,
-                    speed: 1000
+                    speed: 500,
+                    customPaging: function(slider, i) {
+                        return '<button type="button" class="slick-dot"></button>';
+                      },
+                      appendDots: $('.carousel__inner')
                 }
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
 });
